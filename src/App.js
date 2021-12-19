3c8c384de59a3be6d58fa0921ea4be163a9887cf
Sean Stephenson <seanmichaelstephenson@gmail.com>
2021-12-19 12:16:49 -0800

    add wave portal addr

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import dotenv from 'dotenv'
 import './App.css';
+
+dotenv.config();
+
+const { WAVE_PORTAL_ADDRESS } = process.env;
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const contractAddress = WAVE_PORTAL_ADDRESS;
   
   const checkIfWalletIsConnected = async () => {
     try {
@@ -50,6 +57,25 @@ const App = () => {
     }
   }
 
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
@@ -65,7 +91,7 @@ const App = () => {
           I am Sean and I worked at Disney Digital Technology, pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={null}>
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
         
