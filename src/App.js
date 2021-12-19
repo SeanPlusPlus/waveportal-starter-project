6c401a14fc944fef04b4c34433fd0786a476dae4
Sean Stephenson <seanmichaelstephenson@gmail.com>
2021-12-19 11:50:02 -0800

    update app

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I'm Sean! And this is my wave project!
         </div>
 
         <button className="waveButton" onClick={wave}>
