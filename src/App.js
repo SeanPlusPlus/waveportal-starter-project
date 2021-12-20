c5a47e3f9d6602067e7f8273a58f24e3de6dd5e5
Sean Stephenson <seanmichaelstephenson@gmail.com>
2021-12-20 15:27:47 -0800

    lang update

--- src/App.js
+++ src/App.js
@@ -190,8 +190,8 @@ const App = () => {
         </div>
 
         <div className="bio">
-          Hiya there! I am <a href="https://twitter.com/seanplusplus" target="_blank" rel="noopener noreferrer" >Sean</a> and I work at Disney digital media.
-          This is a <a href="https://github.com/SeanPlusPlus/waveportal-starter-project" target="_blank" rel="noopener noreferrer" >prototype I am hacking together</a> based on <a href="https://app.buildspace.so/projects/CO02cf0f1c-f996-4f50-9669-cf945ca3fb0b/lessons/LEe9f04c2e-fe9c-4e87-81b2-efb677a1720c" target="_blank" rel="noopener noreferrer" >this tutorial</a>.
+          Hiya there! I'm <a href="https://twitter.com/seanplusplus" target="_blank" rel="noopener noreferrer" >Sean</a>, and I'm a Sr Software Engineer at Disney digital media.
+          This is a prototype I am <a href="https://github.com/SeanPlusPlus/waveportal-starter-project" target="_blank" rel="noopener noreferrer" >hacking together</a> based on <a href="https://app.buildspace.so/projects/CO02cf0f1c-f996-4f50-9669-cf945ca3fb0b/lessons/LEe9f04c2e-fe9c-4e87-81b2-efb677a1720c" target="_blank" rel="noopener noreferrer" >this tutorial</a>.
           Connect your Ethereum wallet, craft a message, and wave at me!
           To start, you probably should <a href="https://web3hackathon.vercel.app/how-to" target="_blank" rel="noopener noreferrer" >follow this</a> (you can go and mint your very own 🏈 NFT there while you're at it).
         </div>
